bf9199067da7105e91564d29acf1b739ded0903f
alexMartJu <alex.mart.juan@gmail.com>
2025-01-12 20:05:03 +0100

    Modificaciones

--- ‎jest.config.js
+++ ‎jest.config.js
@@ -1,13 +1,15 @@
 export default {
     transform: {
-        '^.+\\.jsx?$': 'babel-jest'
+        '^.+\\.jsx?$': 'babel-jest',
     },
+    extensionsToTreatAsEsm: ['.jsx', '.js'], // Trata estos archivos como ESM
     testEnvironment: 'node',
     transformIgnorePatterns: [
-        'node_modules/(?!(lodash-es)/)'
+        'node_modules/(?!(lodash-es)/)' // Excluye lodash-es de la regla de ignorar
     ],
+    moduleFileExtensions: ['js', 'jsx'],
     testMatch: [
-        "*/src//.test.js"
+        "**/src/**/*.test.js"
     ],
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
-}
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+};
