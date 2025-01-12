fd80999508718a04827d74cc565f6e0682bba7a4
alexMartJu <alex.mart.juan@gmail.com>
2025-01-12 19:12:41 +0100

    Modificacion utils.test.js

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,13 +1,17 @@
 import { expect, test } from '@jest/globals';
+
 import { add, isEven, capitalize } from './utils';
+
 test('add() adds two numbers correctly', () => {
     expect(add(2, 3)).toBe(5);
     expect(add(2, 1)).toBe(3);
 });
+
 test('isEven() checks if a number is even', () => {
     expect(isEven(2)).toBe(true);
     expect(isEven(5)).toBe(false);
 });
+
 test('capitalize() capitalizes the first letter of a string', () => {
     expect(capitalize('hello')).toBe('Hello');
     expect(capitalize('world')).toBe('World');
